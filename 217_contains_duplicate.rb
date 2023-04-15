e8f28b2e3d94d85641777405f66b3de9fb5763d7
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-04-15 14:48:37 +1000

    adding 217 contains duplicate

---
+++ 217_contains_duplicate.rb
@@ -0,0 +1,12 @@
+# @param {Integer[]} nums
+# @return {Boolean}
+def contains_duplicate(nums)
+  unique_nums = nums.uniq
+  nums.length != unique_nums.length
+end
+
+# nums = [1,2,3,1]
+nums = [1,2,3,4]
+# nums = [1,1,1,3,3,4,3,2,4,2]
+
+p contains_duplicate(nums)
