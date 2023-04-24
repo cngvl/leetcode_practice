b973d78b05f7b3ff8c9acb36007c1ae39f8af5bf
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-04-24 16:06:53 +1000

    updating hashmap notes

--- 242_valid_anagram.rb
+++ 242_valid_anagram.rb
@@ -7,6 +7,13 @@ def is_anagram(s, t)
   # return boolean value
 end
 
+# can also use a hashmap to count for each occurance of a given letter
+# the sort feature may not be used in all langs
+# hash = { letter , freq }
+# s_hash = { a = 3, n = 1 ...}
+# t_hash = { a = 3, n = 1 ...}
+# see if s_hash == t_hash
+
 s = 'anagram'
 t = 'nagaram'
 
