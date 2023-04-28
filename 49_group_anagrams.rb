4c731225464b5e441f0969319ceeb8dc975d1213
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-04-28 21:37:41 +1000

    im still stupid

--- 49_group_anagrams.rb
+++ 49_group_anagrams.rb
@@ -1,19 +1,46 @@
+require 'pry-byebug'
+
 # @param {String[]} strs
 # @return {String[][]}
+
+# First solution was dogshit
 def group_anagrams(strs)
+  big_stack = []
+
   strs.each do |str|
-    p str
+    puts str
+    if big_stack.join.split('').sort.include?(str.split('').sort.join)
+      puts "This is old"
+
+    else
+      puts "This is new"
+      small_stack = []
+      small_stack << str
+    end
+    # big_stack << small_stack
+    # byebug
   end
+  # p big_stack
 end
+
+# Solution using answers + inbuilt method
+# def group_anagrams(strs)
+#   strs.sort.group_by { |s| s.chars.sort }.values
+# end
 
 
 strs = ["eat","tea","tan","ate","nat","bat"]
 # >>> [["bat"],["nat","tan"],["ate","eat","tea"]]
+# strs = ["eat","tea","ate"]
 
 # strs = [""]
 # >>> [[""]]
 
 # strs = ["a"]
 # >>> [["a"]]
+
+
+# a = [ ["abc"], ["def"], ["ghi"] ]
+# a = [ ["abc"], ["def", "fed"], ["ghi"] ]
 
 group_anagrams(strs)
