90bc92488456a8bb35d0c4108e9496b7a20931c4
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-05-10 16:06:33 +1000

    starting q724 find pivit index

---
+++ 724_find_pivot_index.py
@@ -0,0 +1,22 @@
+def pivotIndex(nums):
+    # print('x')
+    # Can find the target sum value by the total / 2
+    # Run two loops, seperately?
+        # One going left to right
+        # Other going right to left
+    # Loops continue until they hit the target value
+    # Once they hit the target value, find the index they stopped at
+    # Compare the two, if they match return the match value
+    # If not, return -1
+
+
+
+
+# nums = [1,7,3,6,5,6]
+# >>> 3
+
+# nums = [1,2,3]
+# >>> -1
+
+# nums = [2,1,-1]
+# >>> 0
