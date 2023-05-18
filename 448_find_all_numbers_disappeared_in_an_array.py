e12569d1957bef690ba39b44ed29f1406f033cc6
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-05-18 19:20:29 +1000

    added solution using same logic but different data structure

--- 448_find_all_numbers_disappeared_in_an_array.py
+++ 448_find_all_numbers_disappeared_in_an_array.py
@@ -1,24 +1,38 @@
+# def findDisappearedNumbers(nums):
+#     # Can sort the array of elements
+#     # Loop? through the array, for its range - not looping through the array itself???
+#         #  If the num is not in the array, push it to an answer array
+
+#     # Another option is to make a new array with a complete range, pop out every element (in the range array) that is contained in both
+#     # min(nums)
+#     # max(nums)
+#     nums_range = range(1, len(nums) + 1)
+#     ans_array = []
+#     for number in nums_range:
+#         # print(number)
+#         # print(number in nums)
+#         if number not in nums:
+#             # print(number)
+#             ans_array.append(number)
+
+#     # cheater
+#     # if len(ans_array) == 0:
+#     #     ans_array.append(max(nums) + 1)
+#     return ans_array
+
 def findDisappearedNumbers(nums):
-    # Can sort the array of elements
-    # Loop? through the array, for its range - not looping through the array itself???
-        #  If the num is not in the array, push it to an answer array
+    numDict = {}
+    toReturn = []
 
-    # Another option is to make a new array with a complete range, pop out every element (in the range array) that is contained in both
-    # min(nums)
-    # max(nums)
-    nums_range = range(1, len(nums) + 1)
-    ans_array = []
-    for number in nums_range:
-        # print(number)
-        # print(number in nums)
-        if number not in nums:
-            # print(number)
-            ans_array.append(number)
+    for num in nums:
+        if num not in numDict:
+            numDict[num] = 1
 
-    # cheater
-    # if len(ans_array) == 0:
-    #     ans_array.append(max(nums) + 1)
-    return ans_array
+    for i in range(1 , len(nums) + 1):
+        if i not in numDict:
+            toReturn.append(i)
+
+
 
 nums = [4,3,2,7,8,2,3,1]
 # >>> [5,6]
