7031ef82b4e3c1e366653fdb5a9eb4d9ae05db78
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-05-18 17:45:29 +1000

    added neetcode solution

--- 217_contains_duplicate.rb
+++ 217_contains_duplicate.rb
@@ -10,3 +10,14 @@ nums = [1, 2, 3, 4]
 # nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
 
 p contains_duplicate(nums)
+
+# NeetCode solution
+# class Solution:
+#   def containsDuplicate(self, nums: List[int]) -> bool:
+#       hashset = set()
+
+#       for n in nums:
+#           if n in hashset:
+#               return True
+#           hashset.add(n)
+#       return False
