e80e831e62600725790ebb89e068bcb85ef02b88
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-05-19 15:04:54 +1000

    starting q1189 maximum number of balloons

---
+++ 1189_maximum_number_of_balloons.py
@@ -0,0 +1,25 @@
+def maxNumberOfBalloons(text):
+    # Iterate over the text string
+    # Add some counter for each key letter: b a l o n
+        # if b = 1
+        # a = 1
+        # l = 2
+        # o = 2
+        # n = 1
+    # add 1 to return value
+    # maybe make an array with the stored letters so it can pop out the letters after counted? - not sure how i'd pop out these values
+    # alternative is to make a dictionary but not sure how to remove the key + values after
+    # return int
+
+
+# text = "nlaebolko"
+# >>> 1
+
+# text = "loonbalxballpoon"
+# >>> 2
+
+# text = "leetcode"
+# >>> 0
+
+
+maxNumberOfBalloons(text)
