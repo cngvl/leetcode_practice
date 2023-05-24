996532218b83757cbb7fae0bb86eef1a7ab098f6
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-05-24 14:16:09 +1000

    added josiah's solution

--- 680_valid_palindrome_ii.py
+++ 680_valid_palindrome_ii.py
@@ -59,3 +59,15 @@ print(validPalindrome(s))
 #                 return (skipL == skipL[::-1] or skipR == skipR[::-1])
 #             l, r = l + 1, r - 1
 #         return True
+
+# Josiah's solution:
+# class Solution:
+#     def validPalindrome(self, s: str) -> bool:
+#         def is_pali_range(i, j):
+#             return all(s[i + k] == s[j - k] for k in range(i, j))
+
+#         for i in range(len(s) // 2):
+#             if s[i] != s[~i]:
+#                 j = len(s) - 1 - i
+#                 return is_pali_range(i + 1, j) or is_pali_range(i, j - 1)
+#         return True
