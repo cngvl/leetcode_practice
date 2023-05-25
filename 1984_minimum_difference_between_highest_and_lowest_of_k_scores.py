197bc6a1d2bdcf14602ba404653b28679f36dea1
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-05-25 14:42:30 +1000

    solved with no issues

--- 1984_minimum_difference_between_highest_and_lowest_of_k_scores.py
+++ 1984_minimum_difference_between_highest_and_lowest_of_k_scores.py
@@ -17,27 +17,27 @@ def minimumDifference(nums, k):
     # 'Gap' between the pointers need to be variable in length
     pointer1 = 0
     pointer2 = k - 1
-    # min =
+    minimumDiff = max(nums)
     # Need to interate across the array
         # Also need to protect against going out of bounds
         # Do min = min(min, abs(pointer1 - pointer2)) ?
     while pointer2 < len(nums):
-        print(abs(nums[pointer1] - nums[pointer2]))
+        # print(abs(nums[pointer1] - nums[pointer2]))
         # print(nums[pointer1])
         # print(nums[pointer2])
-        min = min(min, abs(nums[pointer1] - nums[pointer2]))
+        minimumDiff = min(minimumDiff, (abs(nums[pointer1] - nums[pointer2])))
         pointer1 += 1
         pointer2 += 1
 
-    return -1
+    return minimumDiff
 
-# nums = [90]
-# k = 1
+nums = [90]
+k = 1
 # >>> 0
 
-nums = [9,4,1,7]
+# nums = [9,4,1,7]
 # nums = [1,4,7,9]
-k = 2
+# k = 2
 # >>> 2
 # Explanation: There are six ways to pick score(s) of two students:
 # - [9,4,1,7]. The difference between the highest and lowest score is 9 - 4 = 5.
@@ -47,4 +47,4 @@ k = 2
 # - [9,4,1,7]. The difference between the highest and lowest score is 7 - 4 = 3.
 # - [9,4,1,7]. The difference between the highest and lowest score is 7 - 1 = 6.
 
-minimumDifference(nums, k)
+print(minimumDifference(nums, k))
