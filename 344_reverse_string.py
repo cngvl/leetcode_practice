2d5c895c263f48fd750b755e30a97c396d0e9397
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-05-26 15:38:19 +1000

    solved with no issues, also included neetcode solution to check debugger

---
+++ 344_reverse_string.py
@@ -0,0 +1,27 @@
+# class Solution:
+#     def reverseString(self, s: List[str]) -> None:
+#         """
+#         Do not return anything, modify s in-place instead.
+#         """
+# def reverseString(s):
+#     s.reverse()
+
+
+# Kind of cheated but I just used whatever tools were available to me
+# I assume they wanted some pointer going in the leftward direction (but this uses more mem?)
+
+def reverseString(s):
+    l = 0
+    r = len(s) - 1
+    while l <= r:
+        s[l],s[r] = s[r],s[l]
+        l += 1
+        r -= 1
+
+s = ["h","e","l","l","o"]
+# >>> ["o","l","l","e","h"]
+
+# s = ["H","a","n","n","a","h"]
+# >>> ["h","a","n","n","a","H"]
+
+reverseString(s)
