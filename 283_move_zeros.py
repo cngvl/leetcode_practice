949b56fd9b2572486cfd2958633e86ef96aae857
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-05-27 16:21:40 +1000

    finished with no issues - im a genius

--- 283_move_zeros.py
+++ 283_move_zeros.py
@@ -10,18 +10,34 @@
     # One stays at the zeros, while the other
 
 def moveZeroes(nums):
+    pointer1 = 0
+    pointer2 = 1
+    while pointer2 < len(nums):
+        if nums[pointer1] == 0 and nums[pointer2] != 0:
+            nums[pointer1] = nums[pointer2]
+            nums[pointer2] = 0
+            pointer1 += 1
+        elif nums[pointer1] != 0:
+            pointer1 += 1
+        pointer2 += 1
+
     print(nums)
-
-
 
 nums = [0]
 # >>> [0]
 
-nums = [0,1,0,3,12]
+# nums = [0,1,0,3,12]
 
 # 1 0 0 3 12
 # 1 3 0 0 12
 # 1 3 12 0 0
 # >>> [1,3,12,0,0]
+
+
+# nums = [1,0]
+# >>> [1,0]
+
+# nums = [2,1]
+# >>> [2,1]
 
 moveZeroes(nums)
