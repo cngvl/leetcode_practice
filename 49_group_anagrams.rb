eafb145e5ce8ada7e9ec9c62b39ec6720a775a0c
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-05-30 19:54:10 +1000

    cleaned up code a bit

--- 49_group_anagrams.rb
+++ 49_group_anagrams.rb
@@ -8,12 +8,9 @@ def group_anagrams(strs)
   big_stack = []
 
   strs.each do |str|
-    puts str
     if big_stack.join.split('').sort.include?(str.split('').sort.join)
-      puts "This is old"
 
     else
-      puts "This is new"
       small_stack = []
       small_stack << str
     end
@@ -28,17 +25,15 @@ end
 #   strs.sort.group_by { |s| s.chars.sort }.values
 # end
 
-
-strs = ["eat","tea","tan","ate","nat","bat"]
-# >>> [["bat"],["nat","tan"],["ate","eat","tea"]]
-# strs = ["eat","tea","ate"]
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+# >>> [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]
+# strs = ["eat", "tea", "ate"]
 
 # strs = [""]
 # >>> [[""]]
 
 # strs = ["a"]
 # >>> [["a"]]
-
 
 # a = [ ["abc"], ["def"], ["ghi"] ]
 # a = [ ["abc"], ["def", "fed"], ["ghi"] ]
