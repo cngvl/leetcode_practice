0a1e58405c6d166531c2ac5766f87ce2bd7a20af
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-06-02 14:07:08 +1000

    starting q219 contains duplicate ii

---
+++ 219_contains_duplicate_ii.py
@@ -0,0 +1,19 @@
+# class Solution:
+#     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+def containsNearbyDuplicate(nums, k):
+    print(nums)
+
+nums = [1,2,3,1]
+k = 3
+# >>> true
+
+nums = [1,0,1,1]
+k = 1
+# >>> true
+
+nums = [1,2,3,1,2,3]
+k = 2
+# >>> false
+
+
+containsNearbyDuplicate(nums, k)
