4447c9156d2ad9e894f59c8fd6d59445b324027d
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-06-09 23:07:24 +1000

    starting q682 baseball game

---
+++ 682_baseball_game.py
@@ -0,0 +1,21 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        print(operations)
+
+
+
+
+
+ops = ["5","2","C","D","+"]
+# >>> 30
+
+# ops = ["5","-2","4","C","D","9","+","+"]
+# >>> 27
+
+# ops = ["1","C"]
+# >>> 0
+
+
+
+s = Solution()
+s.calPoints(ops)
