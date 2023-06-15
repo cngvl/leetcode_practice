67702cc36f97ca57a0ac10086cc79ef82d61d6ce
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-06-15 15:41:29 +1000

    solved with minimal issues - only formatting

--- 682_baseball_game.py
+++ 682_baseball_game.py
@@ -1,12 +1,32 @@
+# integer x = base score
+# '+' = a new score that is the sum of the previous two
+# 'd' = a new score that is double the previous score
+# 'c' = invalidate the previous score, removing it from the record
+
+
+
 class Solution:
     def calPoints(self, operations: list[str]) -> int:
-        print(operations)
+        # print(operations)
+        record = []
+        for item in operations:
+            # print(item)
+            if item == '+':
+                record.append(record[-1] + record[-2])
+            elif item == 'D':
+                record.append(record[-1] * 2)
+            elif item == 'C':
+                del record[-1]
+            else:
+                record.append(int(item))
+        return sum(record)
 
 
 
 
 
-ops = ["5","2","C","D","+"]
+
+# ops = ["5","2","C","D","+"]
 # >>> 30
 
 # ops = ["5","-2","4","C","D","9","+","+"]
@@ -18,4 +38,4 @@ ops = ["5","2","C","D","+"]
 
 
 s = Solution()
-s.calPoints(ops)
+print(s.calPoints(ops))
