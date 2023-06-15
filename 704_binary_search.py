dd97b190492ec5abdf8d4a55fdbce41d0034600f
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-06-15 16:44:38 +1000

    solved with minimal issues - just syntax

--- 704_binary_search.py
+++ 704_binary_search.py
@@ -1,2 +1,32 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
+    def search(self, nums: list[int], target: int) -> int:
+        # print(nums, target)
+        min = 0
+        max = len(nums) - 1
+        while max >= min:
+            middle = (min + max) // 2
+            if target == nums[middle]:
+                return middle
+            elif target > nums[middle]:
+                min = middle + 1
+            else:
+                max = middle - 1
+        return -1
+
+
+
+
+
+
+nums = [-1,0,3,5,9,12]
+target = 9
+# >>> 4
+
+
+
+# nums = [-1,0,3,5,9,12]
+# target = 2
+# >>> -1
+
+s = Solution()
+print(s.search(nums, target))
