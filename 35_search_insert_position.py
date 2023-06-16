6be283ca75fcb48c2d7b0c030ea1aab6dfec9e03
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-06-16 22:28:22 +1000

    starting q35 search insert position

---
+++ 35_search_insert_position.py
@@ -0,0 +1,22 @@
+class Solution:
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        print(nums, target)
+
+
+
+
+
+nums = [1,3,5,6]
+target = 5
+# >>> 2
+
+# nums = [1,3,5,6]
+# target = 2
+# >>> 1
+
+# nums = [1,3,5,6]
+# target = 7
+# >>> 4
+
+s = Solution()
+s.searchInsert(nums, target)
