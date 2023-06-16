1913ca7e57e670cf49f25aa0c1148b29f4a82d18
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-06-16 23:30:35 +1000

    just a lil stupid

--- 35_search_insert_position.py
+++ 35_search_insert_position.py
@@ -16,17 +16,6 @@ class Solution:
                 max = mid
         return min
 
-        # low, high = 0, len(nums)
-        # while low<high:
-        #     mid = low +(high - low) // 2
-        #     if target > nums[mid]:
-        #         low = mid + 1
-        #     else:
-        #         high = mid
-        # return low
-
-
-
 # nums = [1,3,5,6]
 # target = 5
 # >>> 2
