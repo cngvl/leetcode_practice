db77a37ffb1264d16a357d88469958b3ec662542
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-06-16 23:13:16 +1000

    solved with slight issues, just careless mistakes but logic was mostly fine

--- 35_search_insert_position.py
+++ 35_search_insert_position.py
@@ -1,22 +1,51 @@
 class Solution:
     def searchInsert(self, nums: list[int], target: int) -> int:
-        print(nums, target)
+        # min, max = 0, len(nums) - 1
+        min, max = 0, len(nums)
+        while min < max:
+            # mid = (min + max) // 2
+            mid = min +(max - min) // 2
+            # if target == nums[mid]:
+            #     print("If")
+            #     return mid
+            if target > nums[mid]:
+                print("Elif")
+                min = mid + 1
+            else:
+                print("Else")
+                max = mid
+        return min
+
+        # low, high = 0, len(nums)
+        # while low<high:
+        #     mid = low +(high - low) // 2
+        #     if target > nums[mid]:
+        #         low = mid + 1
+        #     else:
+        #         high = mid
+        # return low
 
 
 
-
-
-nums = [1,3,5,6]
-target = 5
+# nums = [1,3,5,6]
+# target = 5
 # >>> 2
 
 # nums = [1,3,5,6]
 # target = 2
 # >>> 1
 
-# nums = [1,3,5,6]
-# target = 7
+nums = [1,3,5,6]
+target = 7
 # >>> 4
 
+# nums = [1,2,3,4,5,6,7,8,9,10,11,12]
+# target = 6
+# >>> 5
+
+# nums = [1,2,3,4,5,6,7,8,9,10,11,12]
+# target = 2
+# >>> 1
+
 s = Solution()
-s.searchInsert(nums, target)
+print(s.searchInsert(nums, target))
