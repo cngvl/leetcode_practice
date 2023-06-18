b9baa877c0b49a94ec2af2922f28718ed674e17d
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-06-18 17:48:41 +1000

    solved with NO issues - i'm a genius but its slow as fuck

--- 441_arranging_coins.py
+++ 441_arranging_coins.py
@@ -1,13 +1,39 @@
 # Constraints:
 # 1 <= n <= 2^31 - 1
 
+# Need to make some sort of loop that increases by 1 each time, 1 + 2 + 3
+# Can I use (first + last) / 2?
+# Should do some while loop that compares the total number of squares with n
+# Once n is < total, stop the loop?
+# if n is at a specific value (1 3 6 10...) it fills a complete row
+# Maybe a hash?
+
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        print(n)
+        # print(n)
+        totalSquares = 0
+        rowNumber = 0
+        squareHash = {}
+        while n >= totalSquares:
+            rowNumber += 1
+            totalSquares += rowNumber
+            squareHash[rowNumber] = totalSquares
+            # print('*' * rowNumber)
+
+        if n == squareHash[rowNumber]:
+            return rowNumber
+        else:
+            return rowNumber - 1
+
+
+
+
 
 # n = 5
 # >>> 2
 
-
-# n = 8
+n = 8
 # >>> 3
+
+s = Solution()
+print(s.arrangeCoins(n))
