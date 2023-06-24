754f3748277bf9fea6ce2050455485721d7da8cc
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-06-24 16:47:53 +1000

    starting q367 valid perfect square

---
+++ 367_valid_perfect_square.py
@@ -0,0 +1,18 @@
+# Constraints:
+# You must not use any built-in library function, such as sqrt.
+# 1 <= num <= 231 - 1
+
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        print(num)
+
+
+num = 16
+# >>> true
+
+# num = 14
+# >>> false
+
+
+s = Solution()
+s.isPerfectSquare(num)
