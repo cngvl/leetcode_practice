059ede015707b924d412df05d63e1c188a62e033
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-06-26 17:55:40 +1000

    solved with no issues this time - big improvements

--- 724_find_pivot_index.py
+++ 724_find_pivot_index.py
@@ -1,19 +1,19 @@
+# Constraints:
+# 1 <= nums.length <= 10^4
+# -1000 <= nums[i] <= 1000
+
+# One thing I missed out the first time I did this question was that I needed to find the LEFTMOST index
+
+# Initially thought to use two loops, one going leftwards and the other going rightwards
+
 class Solution:
     def pivotIndex(self, nums: list[int]) -> int:
-        print(nums)
-
-
-s = Solution()
-s.nums = [1,7,3,6,5,6]
-# >>> 3
-# 1 8 11 17 22 28
-
-# nums = [1,2,3]
-# >>> -1
-
-# nums = [2,1,-1]
-# >>> 0
-
+        for position in range(0, len(nums)):
+            leftTotal = sum(nums[position::])
+            rightTotal = sum(nums[position::-1])
+            if leftTotal == rightTotal:
+                return position
+        return -1
 
 # def pivotIndex(nums):
 #     # Alternative method is to loop through the array
@@ -33,5 +33,15 @@ s.nums = [1,7,3,6,5,6]
 #             print(j)
 #             right_sum += j
 
+nums = [1,7,3,6,5,6]
+# >>> 3
+# 1 8 11 17 22 28
 
-pivotIndex(nums)
+# nums = [1,2,3]
+# >>> -1
+
+# nums = [2,1,-1]
+# >>> 0
+
+s = Solution()
+s.pivotIndex(nums)
