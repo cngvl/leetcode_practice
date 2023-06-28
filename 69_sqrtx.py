98545e6ad870abbc407b5f7db1c46652ee282b40
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-06-28 14:07:32 +1000

    starting q69 sqrtx

---
+++ 69_sqrtx.py
@@ -0,0 +1,17 @@
+# Constraints:
+# 0 <= x <= 2^31 - 1
+# You must not use any built-in exponent function or operator.
+    # For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        print(x)
+
+x = 8
+# >>> 2
+
+# x = 4
+# >>> 2
+
+s = Solution()
+s.mySqrt(x)
