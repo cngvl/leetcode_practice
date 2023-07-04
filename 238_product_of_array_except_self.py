477acd4a61c8f4d74cda8d430781f01fe4070975
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-07-04 19:55:24 +1000

    unfinished - delete/pop/remove all not acting like how i'd want them to

--- 238_product_of_array_except_self.py
+++ 238_product_of_array_except_self.py
@@ -1,11 +1,33 @@
 # Constraints:
-# 2 <= nums.length <= 10&5
-# -30 <= nums[i] <= 30
-# The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+    # 2 <= nums.length <= 10&5
+    # -30 <= nums[i] <= 30
+    # The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+    # You must write an algorithm that runs in O(n) time and without using the division operation.
+
+# My notes:
+    # O(n) times makes me think that I should run some sort of HashMap solution?
+        # I initially thought that I would do something like
+        # hashMap[index] = everything but the index
+        # hashMap[1] = 234
+        # hashMap[2] = 134
+    # Is there even a need to store the values in a hashmap?
 
 class Solution:
     def productExceptSelf(self, nums: list[int]) -> list[int]:
-        print(nums)
+        # hashMap = {}
+        # answerArray = []
+
+        baseNums = nums
+        for pos in range(len(nums)):
+            print(nums[pos])
+            # baseNums.remove(pos)
+            # del baseNums[pos]
+
+        # for num in nums:
+            # print(nums)
+            # baseNums = nums
+            # baseNums.remove(num)
+            # print(num)
 
 
 
