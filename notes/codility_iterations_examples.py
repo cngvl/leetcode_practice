b36bf3450ee8007e9dcd1dfe0a95b7f4ba12eb3b
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-07-10 19:13:49 +1000

    updated to ignore ds_store

---
+++ notes/codility_iterations_examples.py
@@ -0,0 +1,17 @@
+# factorial = 1
+n = 5
+# for i in range (1, n + 1):
+#     print (factorial)
+#     factorial *= i
+#     # print (i)
+#     #
+
+# print (range (1, n + 1))
+for i in range(1, n + 1):
+    print ('*' * i)
+    # print ('*')
+    for j in range(i):
+        # print ('*' * j)
+        # print ('*')
+        # print ('*' * j)
+        print ('a')
