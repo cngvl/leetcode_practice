bfc0572506fd4ab2053e87af48b6913ec7b8729e
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-07-14 16:57:55 +1000

    solved with slight issues, logic was sound but not sure why solution wasn't accepted

--- 26_remove_duplicates_from_sorted_array-attempt2.py
+++ 26_remove_duplicates_from_sorted_array-attempt2.py
@@ -16,15 +16,47 @@
 # -100 <= nums[i] <= 100
 # nums is sorted in non-decreasing order.
 
+
+# Not sure why I can't do a standard loop
+# But I can potentially use some two pointer method?
+
+# First solution
+while False:
+    class Solution:
+        def removeDuplicates(self, nums: list[int]) -> int:
+            pointer1 = 0
+            pointer2 = 1
+
+            while pointer2 < len(nums):
+                if nums[pointer1] == nums[pointer2]:
+                    pointer2 += 1
+                elif nums[pointer1] < nums[pointer2]:
+                    nums[pointer1 + 1] = nums[pointer2]
+                    pointer1 += 1
+                    pointer2 += 1
+
+            return nums
+
 class Solution:
     def removeDuplicates(self, nums: list[int]) -> int:
+        pointer1 = 1
         print(nums)
 
-nums = [1,1,2]
+        for pointer2 in range(1, len(nums)):
+            if nums[pointer2] != nums[pointer2 - 1]:
+                nums[pointer1] = nums[pointer2]
+                pointer1 += 1
+
+        return pointer1
+
+# nums = [1,1,2]
 # >>> 2, nums = [1,2,_]
 
-# nums = [0,0,1,1,1,2,2,3,3,4]
+nums = [0,0,1,1,1,2,2,3,3,4]
 # >>> 5, nums = [0,1,2,3,4,_,_,_,_,_]
+
+# nums = [0,1,2,3,4,5]
+# >>> 6
 
 s = Solution()
 s.removeDuplicates(nums)
