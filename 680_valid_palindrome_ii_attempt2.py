ba7155b0f346364cad9907cfa331acb6b8564f03
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-07-20 17:42:10 +1000

    unfinished logic is fine? but havent figured out how to make it flexible to account for all edge cases

--- 680_valid_palindrome_ii_attempt2.py
+++ 680_valid_palindrome_ii_attempt2.py
@@ -1,10 +1,40 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        print(s)
+        l = 0
+        r = len(s) - 1
+        skippedChars = 0
 
+        while l < r:
 
+            left_char = s[l]
+            right_char = s[r]
 
-s = "aba"
+            x = s[l - 1:r]
+            string = s[::]
+
+            if s[l] == s[r]:
+                l += 1
+                r -= 1
+            elif s[l] == s[r - 1]:
+                l += 1
+                r -= 2
+                skippedChars += 1
+            elif s[l + 1] == s[r]:
+                l += 2
+                r -= 1
+                skippedChars += 1
+            # elif x[::] == x[::-1]:
+            #     l += 2
+            #     r -= 2
+            else:
+                return False
+
+        if skippedChars > 1:
+            return False
+
+        return True
+
+# s = "aba"
 # >>> true
 
 # s = "abca"
@@ -13,6 +43,23 @@ s = "aba"
 # s = "abc"
 # >>> false
 
+# s = "abcdefghij0jihgfecba"
+# >>> true
 
-s = Solution()
-s.validPalindrome(s)
+# s = "abcdefghij0jihg0fecba"
+# >>> false
+
+# s = "aabdeenddbaagbddeedbaa"
+# >>> false
+
+s = "lcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupucul"
+# >>> true
+
+s = "deeee"
+# >>> true
+
+s = "eeccccbebaeeabebccceea"
+# >>> false
+
+sol = Solution()
+print(sol.validPalindrome(s))
