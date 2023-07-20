cefce5919c03404763466015d17c750e2f81db6a
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-07-20 14:24:40 +1000

    reattempting q680 valid palindrome ii

---
+++ 680_valid_palindrome_ii_attempt2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        print(s)
+
+
+
+s = "aba"
+# >>> true
+
+# s = "abca"
+# >>> true
+
+# s = "abc"
+# >>> false
+
+
+s = Solution()
+s.validPalindrome(s)
