171c44a3370798c538c373fd7c20d327a4c702b5
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-07-24 14:41:11 +1000

    solved q1768 with absolutely no issues - i am a genius

--- 1768_merge_strings_alternately.py
+++ 1768_merge_strings_alternately.py
@@ -2,20 +2,37 @@
 # 1 <= word1.length, word2.length <= 100
 # word1 and word2 consist of lowercase English letters.
 
+# Can use #zip function to loop through both strings at the same time but can also just do a position counter
+    # how to account for remaining letters
+    # might need to do a manual counter method
+# Starting with word1
+# Stack question
+
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        print(word1, word2)
+        # x = (zip(word1, word2))
+        # print(tuple(x))
+        s = ''
+        counter = 0
+        for x,y in zip(word1, word2):
+            counter += 1
+            s += x
+            s += y
 
+        if counter < len(word1):
+            s += word1[counter:]
 
+        if counter < len(word2):
+            s += word2[counter:]
 
+        return s
 
-
-word1 = "abc"
-word2 = "pqr"
+# word1 = "abc"
+# word2 = "pqr"
 # >>> "apbqcr"
 
-# word1 = "ab"
-# word2 = "pqrs"
+word1 = "ab"
+word2 = "pqrs"
 # >>> "apbqrs"
 
 # word1 = "abcd"
