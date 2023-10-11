585930a7e2fb8108ccf38df8237593bf0cc1b83b
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-10-11 17:56:53 +1100

    solved q2215 with no issues - question was very easy

--- 2215_find_the_difference_of_two_arrays.py
+++ 2215_find_the_difference_of_two_arrays.py
@@ -3,20 +3,56 @@
 # 1 <= nums1.length, nums2.length <= 1000
 # -1000 <= nums1[i], nums2[i] <= 1000
 
+# Notes:
+    # Initial thought was to just loop through each element in both arrays and then check if its in the other array but then that's an n2 time complex?
+    # Alternative is to go through each array and 'convert' it to a hash
+    # AND THEN loop through the array / hash to see if its in the opposing hashmap
+        # Takes away some memory but significantly cuts down on the time
+
+    # Need to be careful of empty arrays and arrays that are identical
+    # Another way would be to create a duplicate of both arrays and then remove any identical values?
+        # This is another n2 time complex solution
+
 class Solution:
     def findDifference(self, nums1: list[int], nums2: list[int]) -> list[list[int]]:
-        print(nums1, nums2)
+
+        nums1Hash = {}
+        nums2Hash = {}
+
+        nums2MissingValues = []
+        nums1MissingValues = []
+
+        for num in nums1:
+            if num not in nums1Hash:
+                nums1Hash[num] = True
+
+        for num in nums2:
+            if num not in nums2Hash:
+                nums2Hash[num] = True
+
+
+        for num in nums1Hash:
+            if num not in nums2Hash:
+                nums2MissingValues.append(num)
+
+        for num in nums2Hash:
+            if num not in nums1Hash:
+                nums1MissingValues.append(num)
+
+
+
+        print([nums2MissingValues, nums1MissingValues])
 
 
 
 
-nums1 = [1,2,3]
-nums2 = [2,4,6]
-# Output: [[1,3],[4,6]]
+# nums1 = [1,2,3]
+# nums2 = [2,4,6]
+# >>> [[1,3],[4,6]]
 
 nums1 = [1,2,3,3]
 nums2 = [1,1,2,2]
-# Output: [[3],[]]
+# >>> [[3],[]]
 
 
 s = Solution()
