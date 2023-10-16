5e302a1fb0dcd963b5e979d301769c147b930ec2
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-10-17 00:10:10 +1100

    finished q2390 - question was almost too easy

--- 2390_removing_stars_from_a_string.py
+++ 2390_removing_stars_from_a_string.py
@@ -3,9 +3,26 @@
 # s consists of lowercase English letters and stars *.
 # The operation above can be performed on s.
 
+# Note:
+# The input will be generated such that the operation is always possible.
+    # Wont be given any stars BEFORE any chars have been included in the list?
+# It can be shown that the resulting string will always be unique.
+
 class Solution:
     def removeStars(self, s: str) -> str:
-        print(s)
+        # print(s)
+
+        letterStack = []
+        for char in s:
+            # print(char)
+            if char == "*":
+                # print("*")
+                letterStack.pop()
+            else:
+                letterStack.append(char)
+
+        print(''.join(letterStack))
+        print(letterStack)
 
 
 s = "leet**cod*e"
@@ -14,6 +31,5 @@ s = "leet**cod*e"
 # s = "erase*****"
 # >>> ""
 
-
-s = Solution()
-s.removeStars(s)
+sol = Solution()
+sol.removeStars(s)
