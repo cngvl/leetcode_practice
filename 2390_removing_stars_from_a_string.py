9636f0fb033993fa2cddfba4eede0ccead278629
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-10-16 23:52:57 +1100

    starting q2390 removing stars from a string

---
+++ 2390_removing_stars_from_a_string.py
@@ -0,0 +1,19 @@
+# Constraints:
+# 1 <= s.length <= 105
+# s consists of lowercase English letters and stars *.
+# The operation above can be performed on s.
+
+class Solution:
+    def removeStars(self, s: str) -> str:
+        print(s)
+
+
+s = "leet**cod*e"
+# >>> "lecoe"
+
+# s = "erase*****"
+# >>> ""
+
+
+s = Solution()
+s.removeStars(s)
