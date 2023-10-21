fef32bbb31559856805ecadf357c23548f1dc23f
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-10-21 16:19:10 +1100

    working solution - will try clean up to maybe speed it up

--- 150_evaluate_reverse_polish_notation.py
+++ 150_evaluate_reverse_polish_notation.py
@@ -2,9 +2,36 @@
 # 1 <= tokens.length <= 104
 # tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200].
 
+# Initial issue is that the items within the list are all strings and aren't operators or ints
+    # Might make a hashmap for this?
+# After this step I should be able to do the logic of just taking the [-1] and [-2] indexed items from the stack and doing the math operations on them
+#
+
+
 class Solution:
     def evalRPN(self, tokens: list[str]) -> int:
-        print(tokens)
+        numberStack = []
+
+        for item in tokens:
+            if item == "/":
+                numberStack[-2] = int(numberStack[-2] / numberStack[-1])
+                numberStack.pop()
+            elif item == "+":
+                numberStack[-2] = (numberStack[-2] + numberStack[-1])
+                numberStack.pop()
+            elif item == "-":
+                numberStack[-2] = (numberStack[-2] - numberStack[-1])
+                numberStack.pop()
+            elif item == "*":
+                numberStack[-2] = (numberStack[-2] * numberStack[-1])
+                numberStack.pop()
+            else:
+                numberStack.append(int(item))
+
+        return int(numberStack[0])
+
+
+
 
 tokens = ["2","1","+","3","*"]
 # >>> 9
@@ -16,4 +43,4 @@ tokens = ["2","1","+","3","*"]
 # >>> 22
 
 sol = Solution()
-sol.evalRPN(tokens)
+print(sol.evalRPN(tokens))
