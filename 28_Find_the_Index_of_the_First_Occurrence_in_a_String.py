d025ebc2b2ffba68e89daa13e9f2d17107103562
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-10-23 16:19:03 +1100

    solved with slight issues in logic but small adjustments fixed it

--- 28_Find_the_Index_of_the_First_Occurrence_in_a_String.py
+++ 28_Find_the_Index_of_the_First_Occurrence_in_a_String.py
@@ -2,17 +2,48 @@
 # 1 <= haystack.length, needle.length <= 104
 # haystack and needle consist of only lowercase English characters.
 
+# Notes:
+# can do some two pointer type shit
+# iterate throguh the haystack and needle at same time
+    # if the iteration breaks just move onto the next character for the haystack, but needle resets to 0
+# default returns -1
+
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        print(haystack, needle)
+        letterPos = 0
+        needleCounter = 0
 
-haystack = "sadbutsad"
-needle = "sad"
+        while letterPos < len(haystack):
+
+            haystackLetter = haystack[letterPos]
+            needleLetter = needle[needleCounter]
+
+            if haystack[letterPos] == needle[needleCounter]:
+                needleCounter += 1
+            else:
+                letterPos = letterPos - needleCounter
+                needleCounter = 0
+
+            if needleCounter == len(needle):
+                # print(letterPos - needleCounter + 1)
+                return letterPos - needleCounter + 1
+
+            letterPos += 1
+
+        return -1
+
+
+# haystack = "sadbutsad"
+# needle = "sad"
 # >>> 0
 
-# haystack = "leetcode"
-# needle = "leeto"
+haystack = "leetcode"
+needle = "leeto"
 # >>> -1
+
+# haystack = "mississippi"
+# needle = "issip"
+# >>> 4
 
 sol = Solution()
 sol.strStr(haystack, needle)
