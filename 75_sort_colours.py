83fff860bd23ad0e3757c748adfea019a3aceab2
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-10-24 20:05:49 +1100

    starting q75 sort colours

---
+++ 75_sort_colours.py
@@ -0,0 +1,23 @@
+# Constraints:
+# n == nums.length
+# 1 <= n <= 300
+# nums[i] is either 0, 1, or 2.
+
+# Notes
+    # Empty arrays?
+    # Arrays with only one or two of the listed values
+
+class Solution:
+    def sortColors(self, nums: list[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        print(nums)
+
+
+
+nums = [2,0,2,1,1,0]
+# >>> [0,0,1,1,2,2]
+
+# nums = [2,0,1]
+# >>> [0,1,2]
