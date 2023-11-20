7ccd384fa2c3bcb896a0c1178f6d3a0453d358e0
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-11-20 23:12:31 +1100

    not really solved because this is poorly optimised

--- 739_daily_temperatures.py
+++ 739_daily_temperatures.py
@@ -1,11 +1,31 @@
 # Constraints:
 # 1 <= temperatures.length <= 105
 # 30 <= temperatures[i] <= 100
+    # Does this even change anything in terms of logic?
 
+# Can do a loop that only looks at things downstream
+    # Not sure if there's any cleaner options?
 
 class Solution:
     def dailyTemperatures(self, temperatures: list[int]) -> list[int]:
-        print(temperatures)
+        answer = []
+        # print(temperatures)
+
+        for position in range(0,len(temperatures)):
+            # print(position)
+            currentTemp = temperatures[position]
+            days_till_warmer = 0
+
+            for downstreamPosition in range(position + 1, len(temperatures)):
+                # print(downstreamPosition)
+                ifCheck = temperatures[downstreamPosition] > currentTemp
+                if temperatures[downstreamPosition] > currentTemp:
+                    days_till_warmer = downstreamPosition - position
+                    break
+
+            answer.append(days_till_warmer)
+
+        print(answer)
 
 temperatures = [73,74,75,71,69,72,76,73]
 # >>> [1,1,4,2,1,1,0,0]
@@ -15,8 +35,6 @@ temperatures = [73,74,75,71,69,72,76,73]
 
 # temperatures = [30,60,90]
 # >>> [1,1,0]
-
-
 
 sol = Solution()
 sol.dailyTemperatures(temperatures)
