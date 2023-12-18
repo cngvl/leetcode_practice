fafac57aaf798dd9aa391be707a67f9e82a41a24
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-12-18 21:47:12 +1100

    did not solve - added chatgpt solution to see differences and how to solve

--- 739_daily_temperatures.py
+++ 739_daily_temperatures.py
@@ -15,7 +15,7 @@ class Solution:
             days_till_warmer = 0
 
             for downstreamPosition in range(position + 1, len(temperatures)):
-                ifCheck = temperatures[downstreamPosition] > currentTemp
+                # ifCheck = temperatures[downstreamPosition] > currentTemp
                 if temperatures[downstreamPosition] > currentTemp:
                     days_till_warmer = downstreamPosition - position
                     break
@@ -23,6 +23,21 @@ class Solution:
             answer.append(days_till_warmer)
 
         print(answer)
+
+# class Solution:
+#     def dailyTemperatures(self, temperatures: list[int]) -> list[int]:
+#         answer = [0] * len(temperatures)
+#         stack = []
+
+#         for i in range(len(temperatures)):
+#             while stack and temperatures[i] > temperatures[stack[-1]]:
+#                 prev_index = stack.pop()
+#                 answer[prev_index] = i - prev_index
+
+#             stack.append(i)
+
+#         return answer
+
 
 temperatures = [73,74,75,71,69,72,76,73]
 # >>> [1,1,4,2,1,1,0,0]
