a8b7f079c6342e3618311e7f69f1385b199925b1
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-12-20 23:57:08 +1100

    added classes to help me along

--- 206_reverse_linked_list.py
+++ 206_reverse_linked_list.py
@@ -10,10 +10,39 @@ class ListNode:
         self.val = val
         self.next = next
 
+class LinkedList:
+    def __init__(self):
+        self.head = ListNode()
+
+    def append(self, val):
+        new_node = ListNode(val)
+        current = self.head
+        while current.next != None:
+            current = current.next
+
+        current.next = new_node
+
+    def display(self):
+        elements = []
+        current = self.head
+        while current.next != None:
+            current = current.next
+            elements.append(current.val)
+        print(elements)
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         # Your code here
         pass
+
+
+node = LinkedList()
+node.append(1)
+node.append(2)
+node.append(3)
+node.append(4)
+node.append(5)
+node.display()
 
 
 # Input: head = [1,2,3,4,5]
