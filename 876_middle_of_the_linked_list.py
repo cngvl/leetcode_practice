aeed75afbb7e256064cd86c7078faa4b45b85c89
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-12-25 13:02:53 +1100

    added example solution - im still a genius

--- 876_middle_of_the_linked_list.py
+++ 876_middle_of_the_linked_list.py
@@ -48,6 +48,21 @@ class Solution:
 
         return slowNode
 
+# LeetCode solution
+# Time Complexity :  O(n)
+# Space Complexity : O(1)
+class Solution(object):
+    def middleNode(self, head):
+        # We need two pointers, one is head with one step each iteration, and the other is tmp with two steps each iteration.
+        temp = head
+        while temp and temp.next:
+            # In each iteration, we move head one node forward and we move temp two nodes forward...
+            head = head.next
+            temp = temp.next.next
+        # When temp reaches the last node, head will be exactly at the middle point...
+        return head
+
+
 ll = LinkedList()
 ll.append(1)
 ll.append(2)
