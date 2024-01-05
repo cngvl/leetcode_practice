1af1177a97a2ae499459fb8f00fda1753db755d3
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-05 21:58:12 +1100

    starting q13 roman to integer

---
+++ 13_roman_to_integer.py
@@ -0,0 +1,20 @@
+# Constraints:
+# 1 <= s.length <= 15
+# s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+# It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        pass
+
+s = "III"
+# >>> 3
+
+# s = "LVIII"
+# >>> 58
+
+# s = "MCMXCIV"
+# >>> 1994
+
+sol = Solution()
+sol.romanToInt(s)
