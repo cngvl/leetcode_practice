0ae3424acc38ae5ae8174dbd4549941b4caeda8b
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-13 22:48:38 +1100

    finished q389 find the difference

--- 389_find_the_difference.py
+++ 389_find_the_difference.py
@@ -4,20 +4,59 @@
 # s and t consist of lowercase English letters.
 
 # Need to len(t) is always longer than len(s) - always by 1?
+# can't iterate throuhg both at the same time because not sure where the difference in the two strings will be?
+    # wrong assumption?
+# need to make sure I account for empty strings
+# misinterepted the question
+    # should not have assumed the letters would be placed in the same order
+# need to count the occurance of the letters themselves
+
 
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        pass
+        if len(s) == 0:
+            return t
 
+        sDict = {}
+        tDict = {}
 
+        for letterPos in range(0, len(s)):
+            # ifCheck = s[letterPos] not in sDict
+            if s[letterPos] not in sDict:
+                sDict[s[letterPos]] = 1
+            else:
+                sDict[s[letterPos]] += 1
 
-s = "abcd"
-t = "abcde"
+        print(sDict)
+
+        for letterPos in range(0, len(t)):
+            # ifCheck = t[letterPos] not in tDict
+            if t[letterPos] not in tDict:
+                tDict[t[letterPos]] = 1
+            else:
+                tDict[t[letterPos]] += 1
+
+        print(tDict)
+
+        for key in tDict:
+            if key not in sDict or tDict[key] != sDict[key]:
+                return key
+
+# s = "abcd"
+# t = "abcde"
 # >>> "e"
 
-# s = ""
-# t = "y"
+s = ""
+t = "y"
 # >>> "y"
+
+# s = "abcdefghijklmnopqrstuvwxyz"
+# t = "abcdefghijklmnopqrstuvwxyza"
+# >>> "a"
+
+# s = "abcdefghijklmnopqrstuvwxyz"
+# t = "abcdefghijklmanopqrstuvwxyz"
+# >>> "a"
 
 sol = Solution()
 sol.findTheDifference(s, t)
