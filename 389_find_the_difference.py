4edf77285adfa1687b82a4c84f753647f728e275
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-13 21:22:06 +1100

    starting q389 find the difference

---
+++ 389_find_the_difference.py
@@ -0,0 +1,23 @@
+# Constraints:
+# 0 <= s.length <= 1000
+# t.length == s.length + 1
+# s and t consist of lowercase English letters.
+
+# Need to len(t) is always longer than len(s) - always by 1?
+
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        pass
+
+
+
+s = "abcd"
+t = "abcde"
+# >>> "e"
+
+# s = ""
+# t = "y"
+# >>> "y"
+
+sol = Solution()
+sol.findTheDifference(s, t)
