ce1937b5595a4f9b4fdbe74f6334e6380bf93d33
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-14 22:37:12 +1100

    starting q387 first unique character in a string

---
+++ 387_first_unique_character_in_a_string.py
@@ -0,0 +1,21 @@
+# Constraints:
+    # 1 <= s.length <= 105
+    # s consists of only lowercase English letters.
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        pass
+
+s = "leetcode"
+# >>>  0
+
+# s = "loveleetcode"
+# >>>  2
+
+# s = "aabb"
+# >>> -1
+
+
+
+sol = Solution()
+sol.firstUniqChar(s)
