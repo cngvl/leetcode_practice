255a89d7e69c069a44cd6de0ecda89cc817a08da
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-14 22:47:16 +1100

    solved with no issues

--- 387_first_unique_character_in_a_string.py
+++ 387_first_unique_character_in_a_string.py
@@ -2,17 +2,40 @@
     # 1 <= s.length <= 105
     # s consists of only lowercase English letters.
 
+
+# First thought is to use some stack?
+    # can also use append pop method
+        # might be sketchy if there's more than 2 instances of the letter
+# Might also be able to use a hashMap
+    # First loop through all the letters and add the number of occurances as a keyval pair
+    # return the first key with a val of 1
+
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        pass
+        seenDict = {}
+        for char in s:
+            if char not in seenDict:
+                seenDict[char] = 1
+            else:
+                seenDict[char] += 1
 
-s = "leetcode"
+        print(seenDict)
+
+        for key in seenDict:
+            if seenDict[key] == 1:
+                print(f"returning {s.index(key)} for the letter {key}")
+                return s.index(key)
+
+        print("returning -1 ")
+        return -1
+
+# s = "leetcode"
 # >>>  0
 
 # s = "loveleetcode"
 # >>>  2
 
-# s = "aabb"
+s = "aabb"
 # >>> -1
 
 
