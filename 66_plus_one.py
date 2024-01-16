5d8c6f465dfdccb5ac203f6b7154fb15e6fa88d9
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-16 23:12:33 +1100

    starting q66 plus one

---
+++ 66_plus_one.py
@@ -0,0 +1,23 @@
+
+# Constraints:
+    # 1 <= digits.length <= 100
+    # 0 <= digits[i] <= 9
+    # digits does not contain any leading 0's.
+
+
+class Solution:
+    def plusOne(self, digits: list[int]) -> list[int]:
+        pass
+
+digits = [1,2,3]
+# >>> [1,2,4]
+
+# digits = [4,3,2,1]
+# >>> [4,3,2,2]
+
+# digits = [9]
+# >>> [1,0]
+
+
+sol = Solution()
+sol.plusOne(digits)
