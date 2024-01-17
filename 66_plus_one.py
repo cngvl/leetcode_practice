4b6eeb174f7d2eaabb26cd8e20f30b17508ab6e0
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-17 22:26:02 +1100

    restoring solution - solved with no issues

--- 66_plus_one.py
+++ 66_plus_one.py
@@ -7,7 +7,17 @@
 
 class Solution:
     def plusOne(self, digits: list[int]) -> list[int]:
-        pass
+        digitString = ""
+        digitList = []
+        for number in digits:
+            digitString += str(number)
+
+        digitString = int(digitString) + 1
+
+        for val in str(digitString):
+            digitList.append(int(val))
+
+        return digitList
 
 digits = [1,2,3]
 # >>> [1,2,4]
