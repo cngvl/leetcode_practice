f3171c9ccdbfca66204782fe4c4a85e4c1bdc2da
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-17 22:23:38 +1100

    starting q1207 unique number of occurrences

---
+++ 1207_unique_number_of_occurrences.py
@@ -0,0 +1,22 @@
+
+
+# Constraints:
+    # 1 <= arr.length <= 1000
+    # -1000 <= arr[i] <= 1000
+
+
+class Solution:
+    def uniqueOccurrences(self, arr: list[int]) -> bool:
+        pass
+
+arr = [1,2,2,1,1,3]
+# >>> true
+
+# arr = [1,2]
+# >>> false
+
+# arr = [-3,0,1,-3,1,1,1,-3,10,0]
+# >>> true
+
+sol = Solution()
+sol.uniqueOccurrences(arr)
