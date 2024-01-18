110f6d813957880bcc49bb7c6fbe52d86303f357
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-18 22:01:53 +1100

    starting 455 assign cookies

---
+++ 455_assign_cookies.py
@@ -0,0 +1,19 @@
+# Constraints:
+    # 1 <= g.length <= 3 * 104
+    # 0 <= s.length <= 3 * 104
+    # 1 <= g[i], s[j] <= 231 - 1
+
+class Solution:
+    def findContentChildren(self, g: list[int], s: list[int]) -> int:
+        pass
+
+g = [1,2,3]
+s = [1,1]
+# >>> 1
+
+# g = [1,2]
+# s = [1,2,3]
+# >>> 2
+
+sol = Solution()
+sol.findContentChildren(g,s)
