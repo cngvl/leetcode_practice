0584e7f273087d6baebb69a6ca127d906ff48351
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-18 21:23:22 +1100

    starting q70 climbing stairs

---
+++ 70_climbing_stairs.py
@@ -0,0 +1,16 @@
+# Constraints:
+# 1 <= n <= 45
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        pass
+
+
+n = 2
+# >>> 2
+
+# n = 3
+# >>> 3
+
+sol = Solution()
+sol.climbStairs(n)
