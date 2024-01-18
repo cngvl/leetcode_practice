31c3e3f94a6f677986769928e18e7e18299f4d9d
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-18 21:34:53 +1100

    leaving question for later date, question is out of scope

--- 70_climbing_stairs.py
+++ 70_climbing_stairs.py
@@ -1,10 +1,16 @@
 # Constraints:
 # 1 <= n <= 45
 
+# Hint 1
+# To reach nth step, what could have been your previous steps? (Think about the step sizes)
+
+# Hint1 seems to suggest some form of memoization?
+# https://www.geeksforgeeks.org/count-ways-reach-nth-stair/
+    # Reading this page makes it seem like this question is currently out of scope.
+
 class Solution:
     def climbStairs(self, n: int) -> int:
         pass
-
 
 n = 2
 # >>> 2
