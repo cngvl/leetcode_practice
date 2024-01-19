11e32d53574201274be5e46a6688f9919f024491
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-19 22:21:49 +1100

    starting q136 single number

---
+++ 136_single_number.py
@@ -0,0 +1,21 @@
+# Constraints:
+    # 1 <= nums.length <= 3 * 104
+    # -3 * 104 <= nums[i] <= 3 * 104
+    # Each element in the array appears twice except for one element which appears only once.
+
+
+class Solution:
+    def singleNumber(self, nums: list[int]) -> int:
+        pass
+
+nums = [2,2,1]
+# >>> 1
+
+# nums = [4,1,2,1,2]
+# >>> 4
+
+# nums = [1]
+# >>> 1
+
+sol = Solution()
+sol.singleNumber(nums)
