842f648bb212d9d3ff0df0a33c809221b40e5407
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-20 21:34:19 +1100

    starting q263 ugly number

---
+++ 263_ugly_number.py
@@ -0,0 +1,19 @@
+# Constraints:
+    # -231 <= n <= 231 - 1
+
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        pass
+
+
+n = 6
+# >>> true
+
+# n = 1
+# >>> true
+
+# n = 14
+# >>> false
+
+sol = Solution()
+sol.isUgly(n)
