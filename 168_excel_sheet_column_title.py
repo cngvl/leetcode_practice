64945dc16c47194b0ab7c8a66c8f5ec25179c5c6
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-21 20:26:37 +1100

    starting q168 excel sheet column title

---
+++ 168_excel_sheet_column_title.py
@@ -0,0 +1,18 @@
+# Constraints:
+# 1 <= columnNumber <= 231 - 1
+
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        pass
+
+columnNumber = 1
+# >>> "A"
+
+# columnNumber = 28
+# >>> "AB"
+
+# columnNumber = 701
+# >>> "ZY"
+
+sol = Solution()
+sol.convertToTitle(columnNumber)
