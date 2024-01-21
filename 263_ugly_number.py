26f6df56561ecb54b9aa4956b85523d8f58523a1
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-21 20:23:38 +1100

    couldn't solve - will revisit

--- 263_ugly_number.py
+++ 263_ugly_number.py
@@ -1,9 +1,22 @@
 # Constraints:
     # -231 <= n <= 231 - 1
+    # "positive integer whose prime factors are limited to 2, 3, and 5."
+
+# Rough math problem
+# n needs to % 2 3 and 5, but NOT any other vals
+# use % or //
+# Can't JUST be those vals because it would break for n = 30
 
 class Solution:
     def isUgly(self, n: int) -> bool:
-        pass
+        if n <= 0:
+            return False
+
+        for val in [2,3,5]:
+            while n % val == 0 :
+                n = n // val
+
+        return n == 1
 
 
 n = 6
@@ -12,7 +25,16 @@ n = 6
 # n = 1
 # >>> true
 
+# n = 8
+# >>> true
+
 # n = 14
+# >>> false
+
+# n = 15
+# >>> true
+
+# n = 16
 # >>> false
 
 sol = Solution()
