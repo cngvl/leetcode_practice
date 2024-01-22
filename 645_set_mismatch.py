de47332608c71abfd0d58f094549b00e1a8c8bfe
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-22 20:48:48 +1100

    starting q645 set mismatch

---
+++ 645_set_mismatch.py
@@ -0,0 +1,16 @@
+# Constraints:
+# 2 <= nums.length <= 104
+# 1 <= nums[i] <= 104
+
+class Solution:
+    def findErrorNums(self, nums: list[int]) -> list[int]:
+        pass
+
+nums = [1,2,2,4]
+# >>> [2,3]
+
+# nums = [1,1]
+# >>> [1,2]
+
+sol = Solution()
+sol.findErrorNums(nums)
