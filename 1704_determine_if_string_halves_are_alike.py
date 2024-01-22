219b10261c33738881ab17d74f6a8871880813f1
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-22 22:55:53 +1100

    solved with no issues - can be slightly optimised

--- 1704_determine_if_string_halves_are_alike.py
+++ 1704_determine_if_string_halves_are_alike.py
@@ -3,11 +3,45 @@
     # s.length is even.
     # s consists of uppercase and lowercase letters.
 
+# Will need to standardise input
+# Make a vowel hash
+# Do one loop for counting each half
+# and then need a comparison loop
+
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
-        pass
+        # vowelHash = {}
+        vowelList = ["a","e","i","o","u"]
+        firstHash = {
+            "vowel": 0,
+            "constant": 0
+        }
 
-s = "book"
+        secondHash = {
+            "vowel": 0,
+            "constant": 0
+        }
+
+        s = s.lower()
+        firstHalf = s[0:len(s)//2]
+        secondHalf = s[len(s)//2:]
+
+        for letter in firstHalf:
+            if letter in vowelList:
+                firstHash["vowel"] += 1
+            else:
+                firstHash["constant"] += 1
+
+        for letter in secondHalf:
+            if letter in vowelList:
+                secondHash["vowel"] += 1
+            else:
+                secondHash["constant"] += 1
+
+
+        return firstHash == secondHash
+
+# s = "book"
 # >>> true
 
 s = "textbook"
