73d1f796f6803ddf54b331b2c5785261a34c9927
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-22 22:00:40 +1100

    solved with no issues

--- 645_set_mismatch.py
+++ 645_set_mismatch.py
@@ -2,15 +2,46 @@
 # 2 <= nums.length <= 104
 # 1 <= nums[i] <= 104
 
+# Will probably need some counter with hash
+# Not sure how to find the missing val
+# Can probably figure out 1 to n using range + len(nums)
+# Might be able to remove one loop
+# Can start off using a range loop rather than just looping through nums
+
 class Solution:
     def findErrorNums(self, nums: list[int]) -> list[int]:
-        pass
+        seenHash = {}
+        dupeVal = 0
+        missingVal = 0
 
-nums = [1,2,2,4]
+        for num in nums:
+            if num not in seenHash:
+                seenHash[num] = 1
+            else:
+                seenHash[num] += 1
+
+        for pos in range(1, len(nums) + 1):
+            ifCheck = pos not in seenHash
+            if pos not in seenHash:
+                missingVal = pos
+
+            elif seenHash[pos] == 2:
+                dupeVal = pos
+
+        returnList = [dupeVal, missingVal]
+        print(returnList)
+
+        return returnList
+
+
+# nums = [1,2,2,4]
 # >>> [2,3]
 
 # nums = [1,1]
 # >>> [1,2]
+
+nums = [1,2,3,4,5,6,7,8,9,10,10]
+# >>> [10, 11]
 
 sol = Solution()
 sol.findErrorNums(nums)
