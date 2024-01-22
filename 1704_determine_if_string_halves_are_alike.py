379950c6f8975211df0eb50a0ec8caf48bf480be
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-22 22:03:09 +1100

    starting q1704 determine if string halves are alike

---
+++ 1704_determine_if_string_halves_are_alike.py
@@ -0,0 +1,17 @@
+# Constraints:
+    # 2 <= s.length <= 1000
+    # s.length is even.
+    # s consists of uppercase and lowercase letters.
+
+class Solution:
+    def halvesAreAlike(self, s: str) -> bool:
+        pass
+
+s = "book"
+# >>> true
+
+s = "textbook"
+# >>> false
+
+sol = Solution()
+sol.halvesAreAlike(s)
