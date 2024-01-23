e7c6a4ea37c265e44be328ed7d1f1f1cf06a0fb5
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-23 23:16:43 +1100

    starting q1239

---
+++ 1239_maximum_length_of_a_concatenated_string_with_unique_characters.py
@@ -0,0 +1,20 @@
+
+
+# Constraints:
+    # 1 <= arr.length <= 16
+    # 1 <= arr[i].length <= 26
+    # arr[i] contains only lowercase English letters.
+
+
+class Solution:
+    def maxLength(self, arr: list[str]) -> int:
+        pass
+
+arr = ["un","iq","ue"]
+# >>> 4
+
+# arr = ["cha","r","act","ers"]
+# >>> 6
+
+# arr = ["abcdefghijklmnopqrstuvwxyz"]
+# >>> 26
