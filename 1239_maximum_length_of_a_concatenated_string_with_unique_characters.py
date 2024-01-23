c06c4ffb63ce36bebfd52393d7d5a7e71b124768
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-23 23:33:58 +1100

    this question is cooked - will revisit soon

--- 1239_maximum_length_of_a_concatenated_string_with_unique_characters.py
+++ 1239_maximum_length_of_a_concatenated_string_with_unique_characters.py
@@ -1,14 +1,33 @@
-
-
 # Constraints:
     # 1 <= arr.length <= 16
     # 1 <= arr[i].length <= 26
     # arr[i] contains only lowercase English letters.
 
+# First thought is to use a sliding window? - concats must be sequential
+# 1 <= arr.length <= 16 - How does this help me?
+# Can brute force and check every single combination - is this O(n2)?
+    # Can't even do this because it doesn't merge multiple elements together
+
+# "A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements."
+
+# Maybe I try deleting elements rather than trying to merge them?
+
 
 class Solution:
     def maxLength(self, arr: list[str]) -> int:
-        pass
+        if len(arr) == 1:
+            return len(arr[0])
+
+        leftPointer = 0
+        rightPointer = 1
+        biggestSeen = 0
+        while leftPointer < len(arr):
+            while rightPointer < len(arr):
+                checkWord = arr[leftPointer] + arr[rightPointer]
+
+
+        return biggestSeen
+
 
 arr = ["un","iq","ue"]
 # >>> 4
@@ -18,3 +37,6 @@ arr = ["un","iq","ue"]
 
 # arr = ["abcdefghijklmnopqrstuvwxyz"]
 # >>> 26
+
+sol = Solution()
+sol.maxLength(arr)
