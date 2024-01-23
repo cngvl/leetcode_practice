43081598a4f515669e8c7751e49d2c337d2f0c39
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-23 23:38:24 +1100

    starting q1897

---
+++ 1897_Redistribute_Characters_to_Make_All_Strings_Equal.py
@@ -0,0 +1,17 @@
+# Constraints:
+    # 1 <= words.length <= 100
+    # 1 <= words[i].length <= 100
+    # words[i] consists of lowercase English letters.
+
+class Solution:
+    def makeEqual(self, words: list[str]) -> bool:
+        pass
+
+words = ["abc","aabc","bc"]
+# >>> true
+
+# words = ["ab","a"]
+# >>> false
+
+sol = Solution()
+sol.makeEqual(words)
