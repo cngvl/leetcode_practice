7c8acef2058090e89d156dfc11ea40cc38b377d8
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-24 22:37:46 +1100

    added simple solution - need to revisit math solution

--- 258_Add_Digits.py
+++ 258_Add_Digits.py
@@ -1,9 +1,22 @@
 # Constraints:
     # 0 <= num <= 231 - 1
 
+# Follow up: Could you do it without any loop/recursion in O(1) runtime?
+    # Is there some trick to this?
+
 class Solution:
     def addDigits(self, num: int) -> int:
-        pass
+        # pass
+        while num > 9:
+            count = 0
+            num = str(num)
+            for char in num:
+                count += int(char)
+
+            num = count
+
+        return num
+
 
 num = 38
 # >>> 2
