a117295aae5ec7f7384c2a8b7baecdea667a4b26
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-24 19:12:51 +1100

    starting q258

---
+++ 258_Add_Digits.py
@@ -0,0 +1,15 @@
+# Constraints:
+    # 0 <= num <= 231 - 1
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+        pass
+
+num = 38
+# >>> 2
+
+# num = 0
+# >>> 0
+
+sol = Solution()
+sol.addDigits(num)
