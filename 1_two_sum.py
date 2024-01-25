7969062b89235e867bb9fb50b11cba8d25491498
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-25 23:39:33 +1100

    starting q1

---
+++ 1_two_sum.py
@@ -0,0 +1,28 @@
+# Constraints:
+    # 2 <= nums.length <= 104
+    # -109 <= nums[i] <= 109
+    # -109 <= target <= 109
+    # Only one valid answer exists.
+
+
+# Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        pass
+
+nums = [2,7,11,15]
+target = 9
+# >>> [0,1]
+
+# nums = [3,2,4]
+# target = 6
+# >>> [1,2]
+
+# nums = [3,3]
+# target = 6
+# >>> [0,1]
+
+sol = Solution()
+sol.twoSum(nums, target)
