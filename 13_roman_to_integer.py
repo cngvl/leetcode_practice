5e016309d9299fd98eeb62f047aa313bc6d04791
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-26 00:02:47 +1100

    adding solution from early jan - no issues

--- 13_roman_to_integer.py
+++ 13_roman_to_integer.py
@@ -5,7 +5,25 @@
 
 class Solution:
     def romanToInt(self, s: str) -> int:
-        pass
+        convertedStack = []
+        romanConvert = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        for inputLetter in s:
+            convertedStack.append(romanConvert[inputLetter])
+
+        for numberPos in range(1,len(convertedStack)):
+            if convertedStack[numberPos] > convertedStack[numberPos - 1]:
+                convertedStack[numberPos - 1] *= -1
+
+        return sum(convertedStack)
 
 s = "III"
 # >>> 3
