9d9564e3128ab777b7c6c8f10498a35a1a53ac4e
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-27 21:29:12 +1100

    solved using boyer moore algo

--- 169_majority_element_attempt_2.py
+++ 169_majority_element_attempt_2.py
@@ -3,17 +3,40 @@
     # 1 <= n <= 5 * 104
     # -109 <= nums[i] <= 109
 
+
+# Initialize an element m and a counter c with c = 0
+# For each element x of the input sequence:
+# If c = 0, then assign m = x and c = 1
+# else if m = x, then assign c = c + 1
+# else assign c = c − 1
+# Return m
+
 class Solution:
     def majorityElement(self, nums: list[int]) -> int:
-            # nums.sort()
-            # return nums[math.floor(len(nums) / 2)]
-        pass
+        currentNum = 0
+        counter = 0
+        for num in nums:
+            ifCheck = (counter == 0)
+            elifCheck = (currentNum == num)
+            if counter == 0:
+                currentNum = num
+                counter = 1
+            elif currentNum == num:
+                counter += 1
+            else:
+                counter -= 1
+
+        # print(currentNum)
+        return currentNum
 
 
-# nums = [3,2,3]
+
+
+
+nums = [3,2,3]
 # >>> 3
 
-nums = [2,2,1,1,1,2,2]
+# nums = [2,2,1,1,1,2,2]
 # >>> 2
 
 sol = Solution()
