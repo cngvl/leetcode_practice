a66fa1f13c52fdde849c8830c782e95e9baaf06a
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-28 20:18:32 +1100

    starting q202

---
+++ 202_happy_number.py
@@ -0,0 +1,15 @@
+# Constraints:
+# 1 <= n <= 231 - 1
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        pass
+
+n = 19
+# >>> true
+
+# n = 2
+# >>> false
+
+sol = Solution()
+sol.isHappy(n)
