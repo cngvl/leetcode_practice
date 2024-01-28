c32c21b8077e650701b41fdcc8e2d4f2e3c69077
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-28 21:38:52 +1100

    starting q268

---
+++ 268_missing_number.py
@@ -0,0 +1,19 @@
+# Constraints:
+    # n == nums.length
+    # 1 <= n <= 104
+    # 0 <= nums[i] <= n
+    # All the numbers of nums are unique.
+    # Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+
+class Solution:
+    def missingNumber(self, nums: list[int]) -> int:
+        pass
+
+# nums = [3,0,1]
+# >>> 2
+
+# nums = [9,6,4,2,3,5,7,0,1]
+# >>> 8
+
+# nums = [0,1]
+# >>> 2
