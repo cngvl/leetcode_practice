036f49be8ad83a0534224e0fe25578e5d63116f1
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-28 20:19:22 +1100

    solved without addressing followup

--- 844_Backspace_String_Compare.py
+++ 844_Backspace_String_Compare.py
@@ -4,21 +4,39 @@
 
 # Follow up: Can you solve it in O(n) time and O(1) space?
 
+# Not sure how I can solve the followup question
+# Basic answer is to make a stack for both and then compare at the end
+
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        pass
+        sStack = []
+        tStack = []
 
-sol = Solution()
-sol.backspaceCompare(s,t)
+        for letter in s:
+            if letter != "#":
+                sStack.append(letter)
+            elif letter == "#" and len(sStack) > 0:
+                sStack.pop()
 
-s = "ab#c"
-t = "ad#c"
+        for letter in t:
+            if letter != "#":
+                tStack.append(letter)
+            elif letter == "#" and len(tStack) > 0:
+                tStack.pop()
+
+        return sStack == tStack
+
+# s = "ab#c"
+# t = "ad#c"
 # >>> true
 
 # s = "ab##"
 # t = "c#d#"
 # >>> true
 
-# s = "a#c"
-# t = "b"
+s = "a#c"
+t = "b"
 # >>> false
+
+sol = Solution()
+sol.backspaceCompare(s,t)
