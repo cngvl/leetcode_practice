5e04d6aed625a8e58a7ab9455aacf670fd1d5654
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-29 19:34:32 +1100

    starting q1002

---
+++ 1002_Find_Common_Characters.py
@@ -0,0 +1,17 @@
+# Constraints:
+    # 1 <= words.length <= 100
+    # 1 <= words[i].length <= 100
+    # words[i] consists of lowercase English letters.
+
+class Solution:
+    def commonChars(self, words: list[str]) -> list[str]:
+        pass
+
+words = ["bella","label","roller"]
+# >>> ["e","l","l"]
+
+# words = ["cool","lock","cook"]
+# >>> ["c","o"]
+
+sol = Solution()
+sol.commonChars(words)
