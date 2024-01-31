554ac91bbfa3df67890df24e69006ee08345b152
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-01-31 21:52:17 +1100

    starting q739 attempt2

---
+++ 739_daily_temperatures_attempt_2.py
@@ -0,0 +1,24 @@
+# Constraints:
+# 1 <= temperatures.length <= 105
+# 30 <= temperatures[i] <= 100
+    # Does this even change anything in terms of logic?
+
+
+class Solution:
+    def dailyTemperatures(self, temperatures: list[int]) -> list[int]:
+        answer = []
+
+        print(answer)
+
+
+temperatures = [73,74,75,71,69,72,76,73]
+# >>> [1,1,4,2,1,1,0,0]
+
+# temperatures = [30,40,50,60]
+# >>> [1,1,1,0]
+
+# temperatures = [30,60,90]
+# >>> [1,1,0]
+
+sol = Solution()
+sol.dailyTemperatures(temperatures)
