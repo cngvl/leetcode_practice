ebc7671795adb840bb9ec945773b8ef049667e64
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-02-04 20:41:33 +1100

    starting q326

---
+++ 326_Power_of_Three.py
@@ -0,0 +1,20 @@
+# Constraints:
+# -231 <= n <= 231 - 1
+
+# Follow up: Could you solve it without loops/recursion?
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        pass
+
+n = 27
+# >>> true
+
+# n = 0
+# >>> false
+
+# n = -1
+# >>> false
+
+sol = Solution()
+sol.isPowerOfThree(n)
