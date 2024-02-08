1874f4f571e4d47d432d30f3a84de64001f22fd1
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-02-08 22:30:37 +1100

    starting q345

---
+++ 345_Reverse_Vowels_of_a_String.py
@@ -0,0 +1,18 @@
+
+
+# Constraints:
+    # 1 <= s.length <= 3 * 105
+    # s consist of printable ASCII characters.
+
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        pass
+
+s = "hello"
+# >>> "holle"
+
+# s = "leetcode"
+# >>> "leotcede"
+
+sol = Solution()
+sol.reverseVowels(s)
