cb3d885a1f03c809c4bbc02903698d76fea44bea
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-02-27 20:42:26 +1100

    starting q2810

---
+++ 2810_Faulty_Keyboard.py
@@ -0,0 +1,18 @@
+# Constraints:
+    # 1 <= s.length <= 100
+    # s consists of lowercase English letters.
+    # s[0] != 'i'
+
+
+class Solution:
+    def finalString(self, s: str) -> str:
+        pass
+
+s = "string"
+# >>> "rtsng"
+
+# s = "poiinter"
+# >>> "ponter"
+
+sol = Solution()
+sol.finalString(s)
