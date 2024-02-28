a025f1067f5f286689b3d8de6dbebba94778a9e5
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-02-28 19:55:52 +1100

    finished with no issues

--- 482_License_Key_Formatting.py
+++ 482_License_Key_Formatting.py
@@ -3,9 +3,27 @@
     # s consists of English letters, digits, and dashes '-'.
     # 1 <= k <= 104
 
+# Can work from the end to the front
+    # Don't really need to flip but might make it easier
+
 class Solution:
     def licenseKeyFormatting(self, s: str, k: int) -> str:
-        pass
+        returnString = ''
+        kCount = 0
+
+        for char in s[::-1]:
+            if char == '-':
+                continue
+            ifCheck = (kCount == k)
+            if kCount == k:
+                returnString += f'-{char.upper()}'
+                kCount = 1
+            else:
+                returnString += char.upper()
+                kCount += 1
+
+
+        print(returnString[::-1])
 
 # s = "5F3Z-2e-9-w"
 # k = 4
@@ -14,3 +32,10 @@ class Solution:
 # s = "2-5g-3-J"
 # k = 2
 # >>> "2-5G-3J"
+
+s = "2-5-g-3-J"
+k = 1
+# >>> "2-5-G-3-J"
+
+sol = Solution()
+sol.licenseKeyFormatting(s, k)
