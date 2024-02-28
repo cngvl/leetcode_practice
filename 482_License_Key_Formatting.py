50a7e0b2d2eda1011482a6cd1e228bb2fe2f7264
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-02-28 19:34:54 +1100

    starting q482

---
+++ 482_License_Key_Formatting.py
@@ -0,0 +1,16 @@
+# Constraints:
+    # 1 <= s.length <= 105
+    # s consists of English letters, digits, and dashes '-'.
+    # 1 <= k <= 104
+
+class Solution:
+    def licenseKeyFormatting(self, s: str, k: int) -> str:
+        pass
+
+# s = "5F3Z-2e-9-w"
+# k = 4
+# >>> "5F3Z-2E9W"
+
+# s = "2-5g-3-J"
+# k = 2
+# >>> "2-5G-3J"
