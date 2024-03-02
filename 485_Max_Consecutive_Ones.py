b68457687740080ef2672bb5e51473315ad27bb4
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-03-02 13:08:26 +1100

    starting q485

---
+++ 485_Max_Consecutive_Ones.py
@@ -0,0 +1,16 @@
+# Constraints:
+# 1 <= nums.length <= 105
+# nums[i] is either 0 or 1.
+
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: list[int]) -> int:
+        pass
+
+nums = [1,1,0,1,1,1]
+# >>> 3
+
+# nums = [1,0,1,1,0,1]
+# >>> 2
+
+sol = Solution()
+sol.findMaxConsecutiveOnes(nums)
