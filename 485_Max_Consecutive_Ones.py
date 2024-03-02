d5299d45ca11f0bf40a16f35ed08e6f1d8e7df98
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-03-02 13:16:09 +1100

    solved - stupid easy

--- 485_Max_Consecutive_Ones.py
+++ 485_Max_Consecutive_Ones.py
@@ -2,15 +2,38 @@
 # 1 <= nums.length <= 105
 # nums[i] is either 0 or 1.
 
+# Looks like basic stack / counting question using if else
+
 class Solution:
     def findMaxConsecutiveOnes(self, nums: list[int]) -> int:
-        pass
+        maxConsec = 0
+        currStreak = 0
 
-nums = [1,1,0,1,1,1]
+        for num in nums:
+            if num == 1:
+                currStreak += 1
+                maxConsec = max(currStreak, maxConsec)
+            elif num == 0:
+                currStreak = 0
+
+        print(maxConsec)
+
+        return maxConsec
+
+
+
+
+# nums = [1,1,0,1,1,1]
 # >>> 3
 
-# nums = [1,0,1,1,0,1]
+nums = [1,0,1,1,0,1]
 # >>> 2
+
+nums = [0]
+# >>> 0
+
+nums = [1]
+# >>> 1
 
 sol = Solution()
 sol.findMaxConsecutiveOnes(nums)
