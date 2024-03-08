5233ab450c36084bc54730711b379664ee65dc92
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-03-08 12:08:32 +1100

    VIM IS SOSOSOS HARD

---
+++ 3005_Count_Elements_With_Maximum_Frequency.py
@@ -0,0 +1,13 @@
+# Constraints:
+    # 1 <= nums.length <= 100
+    # 1 <= nums[i] <= 100
+
+class Solution:
+    def maxFrequencyElements(self, nums: list[int]) -> int:
+        pass
+
+# nums = [1,2,2,3,1,4]
+# >>> 4
+
+nums = [1,2,3,4,5]
+# Output: 5
