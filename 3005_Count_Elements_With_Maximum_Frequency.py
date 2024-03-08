130b635803500a33f195c789334e9a4eccaa85a6
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-03-08 16:01:42 +1100

    question was easy but VIM MOTIONS ARE HARD

--- 3005_Count_Elements_With_Maximum_Frequency.py
+++ 3005_Count_Elements_With_Maximum_Frequency.py
@@ -2,12 +2,47 @@
     # 1 <= nums.length <= 100
     # 1 <= nums[i] <= 100
 
+# HashMap iteration question?
+    # Create a hashmap
+    # Loop through that hashmap and create a counter
+        # Might be a way to check for max and then add to counter all in one?
+    # Return countertotal
+
 class Solution:
     def maxFrequencyElements(self, nums: list[int]) -> int:
-        pass
+        numsHash = {}
+        max = 0
+        totalFreq = 0
+
+        for num in nums:
+            if num not in numsHash:
+                numsHash[num] = 1
+            else:
+                numsHash[num] += 1
+
+        # print(numsHash)
+
+        for num in nums:
+            if max < numsHash[num]:
+                max = numsHash[num]
+                totalFreq = 1
+            elif max == numsHash[num]:
+                totalFreq += 1
+
+        print(f'max = {max}')
+
+        print(f'totalFreq = {totalFreq}')
+
+
+
+
+
 
 # nums = [1,2,2,3,1,4]
 # >>> 4
 
 nums = [1,2,3,4,5]
 # Output: 5
+
+sol = Solution()
+sol.maxFrequencyElements(nums)
