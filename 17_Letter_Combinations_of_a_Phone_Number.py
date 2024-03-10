218d85862ae4b925ca01b22ee91b31a855d86d7e
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-03-10 23:09:24 +1100

    starting q17

---
+++ 17_Letter_Combinations_of_a_Phone_Number.py
@@ -0,0 +1,20 @@
+# Constraints:
+# 0 <= digits.length <= 4
+# digits[i] is a digit in the range ['2', '9'].
+
+class Solution:
+    def letterCombinations(self, digits: str) -> list[str]:
+        pass
+
+
+digits = "23"
+# >>> ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+# digits = ""
+# >>> []
+
+# digits = "2"
+# >>> ["a","b","c"]
+
+sol = Solution()
+sol.letterCombinations(digits)
