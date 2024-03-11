f977d50de60fed6d4d641359cd03a5a794d008ea
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-03-11 20:47:14 +1100

    not sure how to solve - added notes

--- 17_Letter_Combinations_of_a_Phone_Number.py
+++ 17_Letter_Combinations_of_a_Phone_Number.py
@@ -2,6 +2,11 @@
 # 0 <= digits.length <= 4
 # digits[i] is a digit in the range ['2', '9'].
 
+# Would this be considered to be a dynamic programming question?
+# 'brute' force methoud would be to just loop through but then the issue comes with a variable number of digits being included
+# Would this be considered to be more like a backtracking?¿
+
+
 class Solution:
     def letterCombinations(self, digits: str) -> list[str]:
         pass
