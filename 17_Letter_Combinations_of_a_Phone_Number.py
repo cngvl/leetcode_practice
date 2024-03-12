d6747b0182a9b5c292818861261a410ad8f3ca26
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-03-12 22:39:18 +1100

    couldnt solve - looked at neetcode

--- 17_Letter_Combinations_of_a_Phone_Number.py
+++ 17_Letter_Combinations_of_a_Phone_Number.py
@@ -9,8 +9,27 @@
 
 class Solution:
     def letterCombinations(self, digits: str) -> list[str]:
-        pass
+        res = []
+        digToChar = { "2" : "abc",
+                    "3" : "def",
+                    "4" : "ghi",
+                    "5" : "jkl",
+                    "6" : "mno",
+                    "7" : "pqrs",
+                    "8" : "tuv",
+                    "9" : "wxyz"}
 
+        def backtrack(i, curStr):
+            if len(curStr) == len(digits):
+                res.append(curStr)
+                return
+            for c in digToChar[digits[i]]:
+                backtrack(i + i, curStr + c)
+
+        if digits:
+            backtrack(0, "")
+
+        return res
 
 digits = "23"
 # >>> ["ad","ae","af","bd","be","bf","cd","ce","cf"]
