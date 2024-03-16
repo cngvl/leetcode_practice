7be5ef3345859578ee3fdf619e329635cdf1bcc3
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-03-16 16:35:58 +1100

    starting q728

---
+++ 728_Self_Dividing_Numbers.py
@@ -0,0 +1,16 @@
+# Constraints:
+    # 1 <= left <= right <= 104
+class Solution:
+    def selfDividingNumbers(self, left: int, right: int) -> list[int]:
+        pass
+
+left = 1
+right = 22
+# >>> 1,2,3,4,5,6,7,8,9,11,12,15,22]
+
+# left = 47
+# right = 85
+# >>> [48,55,66,77]
+
+sol = Solution()
+sol.selfDividingNumbers(left, right)
