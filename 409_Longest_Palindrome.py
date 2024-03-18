755401c9022475f14fc97b868969037559b1a172
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-03-18 14:22:43 +1100

    solved - ezpz question

--- 409_Longest_Palindrome.py
+++ 409_Longest_Palindrome.py
@@ -2,15 +2,55 @@
     # 1 <= s.length <= 2000
     # s consists of lowercase and/or uppercase English letters only.
 
+# the question is what is the longest palindrome that can be BUILT using the input characters
+# needs to be case sensitive a != A
+# Can do some sort of counting?
+    # needs to have multiples of two + a middle char?
+
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        pass
+        charHash = {}
+        returnInt = 0
+        pivitBool = False
 
+        for char in s:
+            if char in charHash:
+                charHash[char] += 1
+            else:
+                charHash[char] = 1
+
+        print(charHash)
+
+        for char in charHash:
+            if charHash[char] >= 2:
+                # pass
+                returnInt += (charHash[char] // 2) * 2
+                charHash[char] = charHash[char] % 2
+            if pivitBool == False and charHash[char] >= 1:
+                returnInt += 1
+                pivitBool = True
+
+        print(charHash)
+        print(returnInt)
+
+        return returnInt
 s = "abccccdd"
 # >>> 7
 
-# s = "a"
+s = "a"
 # >>> 1
+
+s = "aa"
+# >>> 2
+
+s = "aab"
+# >>> 3
+
+# s = "abc"
+# >>> 2
+
+s = "ccc"
+# >>> 3
 
 sol = Solution()
 sol.longestPalindrome(s)
