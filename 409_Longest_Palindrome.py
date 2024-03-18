b2cab0fb134b2336a378daf9285bc6966d7f489b
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-03-18 13:22:46 +1100

    starting q409

---
+++ 409_Longest_Palindrome.py
@@ -0,0 +1,16 @@
+# Constraints:
+    # 1 <= s.length <= 2000
+    # s consists of lowercase and/or uppercase English letters only.
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        pass
+
+s = "abccccdd"
+# >>> 7
+
+# s = "a"
+# >>> 1
+
+sol = Solution()
+sol.longestPalindrome(s)
