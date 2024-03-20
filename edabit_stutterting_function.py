13d618ebe26088af283f3f33a4f8e79e757080be
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-03-20 11:54:59 +1100

    testing edabit

---
+++ edabit_stutterting_function.py
@@ -0,0 +1,19 @@
+# Notes
+# Assume all input is in lower case and at least two characters long.
+
+class Solution:
+    def stutter(self, word:str) -> str:
+        pass
+
+word = "incredible"
+# "in... in... incredible?"
+
+# word = ("enthusiastic")
+# "en... en... enthusiastic?"
+
+# word = ("outstanding")
+# "ou... ou... outstanding?"
+
+
+sol = Solution()
+sol.stutter(word)
