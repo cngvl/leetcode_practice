3651fedacef2a4e48ce7928523d3d59e65912468
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-03-23 23:51:26 +1100

    starting q496 attempt no2

---
+++ 496_Next_Greater_Element_I_attempt_ii.py
@@ -0,0 +1,23 @@
+# Constraints:
+    # 1 <= nums1.length <= nums2.length <= 1000
+    # 0 <= nums1[i], nums2[i] <= 104
+    # All integers in nums1 and nums2 are unique.
+    # All the integers of nums1 also appear in nums2.
+
+# Follow up: Could you find an O(nums1.length + nums2.length) solution?
+
+class Solution:
+    def nextGreaterElement(self, nums1: list[int], nums2: list[int]) -> list[int]:
+        pass
+
+sol = Solution()
+
+nums1 = [4,1,2]
+nums2 = [1,3,4,2]
+# >>> [-1,3,-1]
+sol.nextGreaterElement(nums1, nums2)
+
+nums1 = [2,4]
+nums2 = [1,2,3,4]
+# >>> [3,-1]
+sol.nextGreaterElement(nums1, nums2)
