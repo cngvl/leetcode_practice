7cb337e295e5453849c993e8748c90cb2165d3a6
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-03-26 21:20:21 +1100

    added faster solutions

--- 496_Next_Greater_Element_I_attempt_ii.py
+++ 496_Next_Greater_Element_I_attempt_ii.py
@@ -24,6 +24,48 @@ class Solution:
                 returnArray.append(-1)
 
         print(returnArray)
+        return returnArray
+
+
+    # NeetCode solution for brute force method
+    def nextGreaterElement2(self, nums1: list[int], nums2: list[int]) -> list[int]:
+        # O(n * m)
+        nums1Idx = { n:i for i,n in enumerate(nums1) }
+        res = [-1] * len(nums1)
+
+        for i in range(len(nums2)):
+            # skips vals that aren't in both lists
+            if nums2[i] not in nums1Idx:
+                continue
+            # (i + 1) means that it will only check the elements downstream of the array
+            for j in range(i + 1, len(nums2)):
+                if nums2[j] > nums2[i]:
+                    idx = nums1Idx[nums2[i]]
+                    res[idx] = nums2[j]
+                    break
+        return res
+
+    # NeetCode stack solution
+    # Addresses followup Q
+    # Space complex of 0(nums1)
+    def nextGreaterElement(self, nums1: list[int], nums2: list[int]) -> list[int]:
+        nums1Idx = { n:i for i,n in enumerate(nums1) }
+        res = [-1] * len(nums1)
+        stack = []
+
+        for i in range(len(nums2)):
+            cur = nums2[i]
+
+            while stack and cur > stack[-1]:
+                val = stack.pop()
+                idx = nums1Idx[val]
+                res[idx] = cur
+
+            if cur in nums1Idx:
+                stack.append(cur)
+
+        return res
+
 
 sol = Solution()
 
