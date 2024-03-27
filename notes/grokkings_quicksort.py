6b36e65c632005d8c3896c915a74c40ee53d6055
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-03-27 21:21:02 +1100

    basic example for quicksort algo

---
+++ notes/grokkings_quicksort.py
@@ -0,0 +1,14 @@
+def quicksort(arr):
+    # Base case
+    # Arrays with 0 or 1 element are already sorted
+    if len(arr) < 2:
+        return arr
+    else:
+        pivot = arr[0]
+        less = [i for i in arr[1:] if i <= pivot]
+
+        greater = [i for i in arr[1:] if i > pivot]
+
+    return quicksort(less) + [pivot] + quicksort(greater)
+
+print(quicksort([10,5,2,3]))
