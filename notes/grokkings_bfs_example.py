314ffad119755e417a8e8f05272814f7ad6af29f
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-03-29 22:28:19 +1100

    working exmaple of bfs

--- notes/grokkings_bfs_example.py
+++ notes/grokkings_bfs_example.py
@@ -1,16 +1,35 @@
 from collections import deque
 
-search_queue = deque()
-graph = {}
-search_queue += graph["you"]
+# search_queue = deque()
+# graph = {}
+# search_queue += graph["you"]
 
 def person_is_seller(name):
     return name[-1] == 'm'
 
-while search_queue:
-    person = search_queue.popleft()
-    if person_is_seller(person):
-        print(f'{person} + "is a mango seller"')
-        # return True
-    else:
-        search_queue += graph[person]
+def search(name):
+    search_queue = deque()
+    graph = {
+        'You': ['Alice', 'Bob', 'Claire'],
+        'Bob': ['Anuj', 'Peggy'],
+        'Alice': ['Peggy'],
+        'Claire': ['Thom', 'Jonny'],
+        'Anuj': [],
+        'Peggy': [],
+        'Thom': [],
+        'Jonny': []
+    }
+    search_queue += graph[name]
+    searched = []
+    while search_queue:
+        person = search_queue.popleft()
+        if person not in searched:
+            if person_is_seller(person):
+                print(f'{person} is a mango seller')
+                return True
+            else:
+                search_queue += graph[person]
+                searched.append(person)
+    return False
+
+print(search("You"))
