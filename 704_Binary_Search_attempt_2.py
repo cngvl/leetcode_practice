33a7d17fe814a429027d05683a430ba9381f4cf6
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-04-03 21:56:05 +1100

    solved with minimal issues

--- 704_Binary_Search_attempt_2.py
+++ 704_Binary_Search_attempt_2.py
@@ -6,17 +6,31 @@
 
 class Solution:
     def search(self, nums: list[int], target: int) -> int:
-        pass
+        low = 0
+        high = len(nums) - 1
 
+        while low < high:
+            mid = (low + high) // 2
+            ifCheck = (target == nums[mid])
+            numsCheck = nums[mid]
+            elifCheck = (target > nums[mid])
+
+            if target == nums[mid]:
+                return mid
+            elif target > nums[mid]:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return -1
 
 sol = Solution()
 
 nums = [-1,0,3,5,9,12]
 target = 9
 # >>> 4
-sol.search(nums, target)
+print(sol.search(nums, target))
 
-# nums = [-1,0,3,5,9,12]
-# target = 2
+nums = [-1,0,3,5,9,12]
+target = 2
 # >>> -1
-sol.search(nums, target)
+print(sol.search(nums, target))
