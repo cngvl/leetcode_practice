cc04c9de3b4dfae453965a7cc18da1abf7fd1b95
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-04-03 21:44:34 +1100

    starting q704 attempt no2

---
+++ 704_Binary_Search_attempt_2.py
@@ -0,0 +1,22 @@
+# Constraints:
+    # 1 <= nums.length <= 104
+    # -104 < nums[i], target < 104
+    # All the integers in nums are unique.
+    # nums is sorted in ascending order.
+
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        pass
+
+
+sol = Solution()
+
+nums = [-1,0,3,5,9,12]
+target = 9
+# >>> 4
+sol.search(nums, target)
+
+# nums = [-1,0,3,5,9,12]
+# target = 2
+# >>> -1
+sol.search(nums, target)
