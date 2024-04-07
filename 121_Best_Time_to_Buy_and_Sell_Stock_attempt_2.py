929216fbfa4b196a4a92c94c64afce39109c1295
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-04-07 21:46:12 +1000

    starting q121 attempt no2

---
+++ 121_Best_Time_to_Buy_and_Sell_Stock_attempt_2.py
@@ -0,0 +1,16 @@
+# Constraints:
+    # 1 <= prices.length <= 105
+    # 0 <= prices[i] <= 104
+
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        pass
+
+sol = Solution()
+prices = [7,1,5,3,6,4]
+# >>> 5
+sol.maxProfit(prices)
+
+# prices = [7,6,4,3,1]
+# >>> 0
+sol.maxProfit(prices)
