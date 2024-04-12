58e48cdb4ba3ce76932b01d595b9f4e65e474ab9
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-04-12 17:55:24 +1000

    solved using cheater solution

--- 141_Linked_List_Cycle.py
+++ 141_Linked_List_Cycle.py
@@ -38,10 +38,40 @@ class LinkedList:
 # First thought would be to see if there's an infinite loop?
     # Would this need some sort of counter¿? - there's a constraint to how many nodes there are in the LList but it's 10^4
     # This runs in O(n) but sounds a bit silly - it does satisfy the constant memory follow up Q but
+# How does the constraint of 'pos is -1 or avalid index in the linked-list' change anything
+    # I don't even know what it means to be honest bruvs
+
+# The solution worked¿?¿?
+# Question is listed as HashMap, LList and TwoPointers
+    # The faster solutions used the fast + slow pointer method but I don't actually get the logic
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        pass
+        numberNodesSeen = 0
+
+        if not head:
+            return False
+
+        curr = head.next
+        while curr:
+            numberNodesSeen += 1
+            if numberNodesSeen > 10_000:
+                return True
+
+            curr = curr.next
+
+        return False
+
+    def hasCycle2(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+
+        return False
 
 sol = Solution()
 
