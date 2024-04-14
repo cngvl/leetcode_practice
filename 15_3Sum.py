1e965c08e47e21b9755d187eac80a7c291c70f32
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-04-14 21:41:32 +1000

    starting q15

---
+++ 15_3Sum.py
@@ -0,0 +1,16 @@
+# Constraints:
+    # 3 <= nums.length <= 3000
+    # -105 <= nums[i] <= 105
+
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        pass
+
+# nums = [-1,0,1,2,-1,-4]
+# >>> [[-1,-1,2],[-1,0,1]]
+
+# nums = [0,1,1]
+# >>> []
+
+# nums = [0,0,0]
+# >>> [[0,0,0]]
