e15f81948ff05a76584f87a82a5f09b69ca65a0b
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-04-20 21:25:28 +1000

    starting q1299 attempt no2

---
+++ 1299_Replace_Elements_with_Greatest_Element_on_Right_Side_attempt_2.py
@@ -0,0 +1,17 @@
+# Constraints:
+    # 1 <= arr.length <= 104
+    # 1 <= arr[i] <= 105
+
+
+class Solution:
+    def replaceElements(self, arr: list[int]) -> list[int]:
+        pass
+
+sol = Solution()
+arr = [17,18,5,4,6,1]
+sol.replaceElements(arr)
+# >>> [18,6,6,6,1,-1]
+
+# arr = [400]
+# >>> [-1]
+sol.replaceElements(arr)
