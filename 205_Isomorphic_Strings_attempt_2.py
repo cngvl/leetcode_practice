6d6d70f560fd60b0ede4c2b5f59526eb25070f69
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-04-22 15:05:54 +1000

    starting q205 attempt 2

---
+++ 205_Isomorphic_Strings_attempt_2.py
@@ -0,0 +1,24 @@
+# Constraints:
+    # 1 <= s.length <= 5 * 104
+    # t.length == s.length
+    # s and t consist of any valid ascii character.
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        pass
+
+sol = Solution()
+
+s = "egg"
+t = "add"
+sol.isIsomorphic(s, t)
+# >>> true
+
+# s = "foo"
+# t = "bar"
+# >>> false
+
+
+# s = "paper"
+# t = "title"
+# >>> true
