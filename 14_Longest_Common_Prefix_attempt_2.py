9321a17f64d6c58354cd33e1636815b7e36d59c0
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-04-30 22:27:54 +1000

    starting q14 attempt no2

---
+++ 14_Longest_Common_Prefix_attempt_2.py
@@ -0,0 +1,16 @@
+# Constraints:
+    # 1 <= strs.length <= 200
+    # 0 <= strs[i].length <= 200
+    # strs[i] consists of only lowercase English letters.
+
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        pass
+
+sol = Solution()
+strs = ["flower","flow","flight"]
+sol.longestCommonPrefix(strs)
+# >>> "fl"
+
+# strs = ["dog","racecar","car"]
+# >>> ""
