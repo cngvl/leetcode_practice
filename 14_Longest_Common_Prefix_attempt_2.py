813ddc20b5f9a37409702bc52da6c9a2677b197c
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-05-04 22:29:31 +1000

    unfinished but im a sick dog

--- 14_Longest_Common_Prefix_attempt_2.py
+++ 14_Longest_Common_Prefix_attempt_2.py
@@ -3,14 +3,44 @@
     # 0 <= strs[i].length <= 200
     # strs[i] consists of only lowercase English letters.
 
+# Second attempt at this question but remember the general solution to solving the q
+# Need to iterate through all the words in str at the same time
+# First attempt used a stack method but there might be a way to solve without it to reduce mem need
+
 class Solution:
     def longestCommonPrefix(self, strs: list[str]) -> str:
-        pass
+        minLength = len(strs[0])
+        returnString = ''
+        for word in strs:
+            minLength = min(minLength, len(word))
+
+        # print(minLength)
+        for letterPos in range(minLength):
+            smallStack = []
+            for word in strs:
+                if word[letterPos] not in smallStack:
+                    smallStack.append(word[letterPos])
+            if len(smallStack) > 1:
+                print(returnString)
+            else:
+                returnString += smallStack[0]
+
+
 
 sol = Solution()
 strs = ["flower","flow","flight"]
 sol.longestCommonPrefix(strs)
 # >>> "fl"
 
-# strs = ["dog","racecar","car"]
+strs = ["dog","racecar","car"]
+sol.longestCommonPrefix(strs)
 # >>> ""
+
+# Failed test case
+strs = [""]
+sol.longestCommonPrefix(strs)
+# >>> ""
+
+strs = ["a"]
+sol.longestCommonPrefix(strs)
+# >>> "a"
