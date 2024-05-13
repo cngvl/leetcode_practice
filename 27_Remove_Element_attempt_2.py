7b0afe04308157129e6987588fbbda9be42f98a1
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-05-13 18:59:55 +1000

    kinda solved - odd question

--- 27_Remove_Element_attempt_2.py
+++ 27_Remove_Element_attempt_2.py
@@ -3,6 +3,10 @@
     # 0 <= nums[i] <= 50
     # 0 <= val <= 100
 
+# Will need to consider what happens when nums length is 0
+# What happens if the only value in nums is the target val?
+# Does sorting the input nums help with anything? - Brings all the vals together?
+
 class Solution(object):
     def removeElement(self, nums, val):
         """
@@ -10,14 +14,50 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        print('x')
+        print(nums)
+
+        # if len(nums) == 0:
+        #     return 0
+
+        # while nums[rightPointer] == val:
+        #     rightPointer -= 1
+
+        # while leftPointer < len(nums):
+        #     if nums[leftPointer] == val:
+        #         temp = nums[leftPointer]
+        #         nums[leftPointer] = nums[rightPointer]
+        #         nums[rightPointer] = temp
+        #         rightPointer -= 1
+        #     leftPointer += 1
+        left_pointer = 0
+
+        for right_pointer in range(len(nums)):
+            if nums[right_pointer] != val:
+                nums[left_pointer] = nums[right_pointer]
+                left_pointer += 1
+
+        print(nums)
+
+
 
 sol = Solution()
-nums = [3,2,2,3]
-val = 3
-sol.removeElement(nums, val)
+# nums = [3,2,2,3]
+# val = 3
+# sol.removeElement(nums, val)
 # >>> 2, nums = [2,2,_,_]
 
-# nums = [0,1,2,2,3,0,4,2]
-# val = 2
+nums = [0,1,2,2,3,0,4,2]
+val = 2
+sol.removeElement(nums, val)
 # >>> 5, nums = [0,1,4,0,3,_,_,_]
+
+# Failed test case
+# nums = []
+# val = 0
+# sol.removeElement(nums, val)
+# >>> 0?
+
+# Fails because the initial while loop results in the index error
+# nums = [1]
+# val = 1
+# sol.removeElement(nums, val)
