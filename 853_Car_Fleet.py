e30425effa261f22e528f45c2fd3ea37cf9ea113
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-05-17 14:20:19 +1000

    starting q853

---
+++ 853_Car_Fleet.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def carFleet(self, target, position, speed):
+        """
+        :type target: int
+        :type position: List[int]
+        :type speed: List[int]
+        :rtype: int
+        """
+        pass
+
+sol = Solution()
+target = 12
+position = [10,8,0,5,3]
+speed = [2,4,1,1,3]
+sol.carFleet(target, position, speed)
+# >>> 3
+
+# target = 10
+# position = [3]
+# speed = [3]
+# sol.carFleet(target, position, speed)
+# >>> 1
+
+# target = 100
+# position = [0,2,4]
+# speed = [4,2,1]
+# sol.carFleet(target, position, speed)
+# >>> 1
